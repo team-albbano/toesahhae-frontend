1ab8574bafb07edf16a016e3b18d7e4dc5497981
moonchaeyeon <sso07069@gmail.com>
2023-09-23 00:17:18 +0900

    [feat] Configure TypeScript and ESLint for importing SVGs as ReactComponent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,46 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  parser: '@typescript-eslint/parser',
+  // env: {
+  //   browser: true,
+  //   es2021: true,
+  // },
   extends: [
-    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
   ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  // overrides: [
+  //   {
+  //     env: {
+  //       node: true,
+  //     },
+  //     files: ['.eslintrc.{js,cjs}'],
+  //     parserOptions: {
+  //       sourceType: 'script',
+  //     },
+  //   },
+  // ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2021,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // JSX 태그를 지원
+    },
   },
-  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect', // 설치된 React의 버전을 자동으로 감지
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     quotes: ['error', 'single', { avoidEscape: true }],
     indent: ['error', 2],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': 'error', // Prettier 규칙을 위반할 때 오류를 출력
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // 원하는 TypeScript 규칙을 추가
+    'react/react-in-jsx-scope': 'off', // React 17 이상에서는 필요 없는 규칙
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo className="App-logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
