e53848076702260beb625b0c9ac661100d6c5d17
moonchaeyeon <sso07069@gmail.com>
2023-09-23 01:15:42 +0900

    [feat] Add initial SCSS configuration for global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { ReactComponent as Logo } from './logo.svg';
-import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-content">
+        <span>퇴사해</span>
+      </div>
     </div>
   );
 }
