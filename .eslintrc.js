28d3b82bfec986080d93397bc1db60e65048b96e
moonchaeyeon <sso07069@gmail.com>
2023-09-24 12:08:20 +0900

    [feat] Install react-query and update ESLint config for its usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  // env: {
-  //   browser: true,
-  //   es2021: true,
-  // },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended'
   ],
-  // overrides: [
-  //   {
-  //     env: {
-  //       node: true,
-  //     },
-  //     files: ['.eslintrc.{js,cjs}'],
-  //     parserOptions: {
-  //       sourceType: 'script',
-  //     },
-  //   },
-  // ],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
@@ -33,7 +19,7 @@ module.exports = {
       version: 'detect', // 설치된 React의 버전을 자동으로 감지
     },
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier', '@tanstack/query'],
   rules: {
     quotes: ['error', 'single', { avoidEscape: true }],
     indent: ['error', 2],
@@ -42,5 +28,7 @@ module.exports = {
     'prettier/prettier': 'error', // Prettier 규칙을 위반할 때 오류를 출력
     '@typescript-eslint/explicit-module-boundary-types': 'off', // 원하는 TypeScript 규칙을 추가
     'react/react-in-jsx-scope': 'off', // React 17 이상에서는 필요 없는 규칙
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/prefer-query-object-syntax": "error"
   },
 };
